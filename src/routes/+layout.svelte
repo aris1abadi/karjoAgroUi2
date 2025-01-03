3773368191b9d3c38c1846567c39d2ee19b33271
aris1abadi <aris1abadi@gmail.com>
2025-01-03 07:34:32 +0700

    new file:   package.json 	new file:   postcss.config.js 	new file:   src/app.css 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/demo.spec.js 	new file:   src/lib/index.js 	new file:   src/lib/stores.js 	new file:   src/routes/+layout.js 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   static/bt1.png 	new file:   static/bt2.png 	new file:   static/favicon.ico 	new file:   static/no-wifi.png 	new file:   static/robots.txt 	new file:   static/setup2.png 	new file:   static/tumbuh1.jpeg 	new file:   static/wifi.png 	new file:   svelte.config.js 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+	import '../app.css';
+
+	
+	</script>
+	
+	<div class="app mainbg">
+		<main>
+			<slot></slot>
+		</main>
+	
+		
+	</div>	
+	
+	<style>
+		.app {
+			display: flex;
+			flex-direction: column;
+			min-height: 100vh;
+		}
+		.mainbg {
+			width: 100%; /* Lebar elemen 100% dari lebar kontainer */
+			height: 100%; /* Tinggi elemen */
+			background-image: url("/tumbuh1.jpeg"); /* URL gambar */
+			background-size: cover; /* Sesuaikan gambar agar menutupi elemen */
+			background-position: center; /* Pusatkan gambar */
+	
+			background-repeat: no-repeat; /* Jangan ulangi gambar */
+		}
+	
+		main {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			padding: 1rem;
+			width: 100%;
+			max-width: 64rem;
+			margin: 0 auto;
+			box-sizing: border-box;
+		}
+	
+		
+	</style>
+	
